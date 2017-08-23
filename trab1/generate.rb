eddbc5f4649cee0db86711317327a158623bf75c
Schweigert <fleyhe0@gmail.com>
2017-08-23 12:50:13 -0300

    Numeros para os testes

---
+++ trab1/generate.rb
@@ -0,0 +1,37 @@
+# Gera os arquivos de entrada
+
+quantia = [25000,50000,100000,1000000]
+
+# na.in -> Crescente
+# nb.in -> Decrescente
+# nc.in -> Aleatorio
+# nd.in -> Aleatório com desvio gigante
+
+for i in quantia
+	a = File.open "#{i}a.in", "w"
+	b = File.open "#{i}b.in", "w"
+	c = File.open "#{i}c.in", "w"
+	d = File.open "#{i}d.in", "w"
+
+	# Posicao do desvio
+	pos = rand(i)
+
+	i.times do |n|
+		# Crescente
+		a.puts(n+1)
+		# Decrescente
+		b.puts(i-n+1)
+		# Aleatorio
+		c.puts(rand(i))
+		# Aleatório com Desvio
+		if pos == n
+			d.puts i*100
+		else
+			d.puts rand(i)
+		end
+	end
+	a.close
+	b.close
+	c.close
+	d.close
+end
