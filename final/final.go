944eae443a3762adbc5d97ba912d036e60b857a6
Marlon Henry Schweigert <fleyhe0@gmail.com>
2017-10-20 19:02:07 -0200

    Crivo finished

--- final/final.go
+++ final/final.go
@@ -1,9 +1,51 @@
 package main
 
-func crivo() []int {
-  tabela := make([]int, 100000)
+import "fmt"
 
-  return tabela
+const (
+  TAM_CRIVO = 100000
+)
+
+// Retorna uma lista de números primos
+func crivo(n int) []int {
+  tabela := make([]bool, n)
+
+  // Inicia a tabela
+  for i := 0; i < len(tabela); i++ {
+    tabela[i] = true
+  }
+
+  // 0 e 1 não são primos
+  tabela[0] = false
+  tabela[1] = false
+
+  // Quantos primos encontrou
+  np := 0
+
+  for i := 0; i < len(tabela); i++ {
+    if !tabela[i]{
+      continue
+    }
+
+    np++
+    for j := i+1; j < len(tabela); j++ {
+      if j%i == 0 {
+        tabela[j] = false
+      }
+    }
+  }
+
+  ret := make([]int, np)
+  np = 0
+
+  for i := 0; i < len(tabela); i++ {
+    if tabela[i] {
+      ret[np] = i
+      np++
+    }
+  }
+
+  return ret
 }
 
 func main() {
